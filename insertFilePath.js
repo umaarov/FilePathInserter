341f3c528635323e18a68310bff7975be38731e0
Ismoiljon <hs.umarov21@gmail.com>
2024-09-05 09:32:03 +0500

    first commit

---
+++ insertFilePath.js
@@ -0,0 +1,46 @@
+const fs = require ('fs');
+const path = require ('path');
+const filePath = process.argv[2];
+let content = fs.readFileSync (filePath, 'utf8');
+const extension = path.extname (filePath);
+let commentSyntax = '';
+function getCommentSyntax (extension, content) {
+  switch (extension) {
+    case '.php':
+      if (content.trim ().startsWith ('<?php')) {
+        return '<!--';
+      } else {
+        return '//';
+      }
+    case '.js':
+    case '.ts':
+    case '.cpp':
+    case '.java':
+    case '.cs':
+    case '.go':
+      return '//';
+    case '.py':
+    case '.sh':
+    case '.rb':
+    case '.pl':
+    case '.yml':
+    case '.yaml':
+    case '.env':
+    case '.toml':
+      return '#';
+    case '.html':
+    case '.xml':
+      return '<!--';
+    case '.css':
+    case '.scss':
+      return '/*';
+    default:
+      return '//';
+  }
+}
+commentSyntax = getCommentSyntax (extension, content);
+const relativeFilePath = path.relative (process.cwd (), filePath);
+const comment = `${commentSyntax} ${relativeFilePath}\n`;
+if (!content.startsWith (comment)) {
+  fs.writeFileSync (filePath, comment + content, 'utf8');
+}
